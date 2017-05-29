0bd4844b9e36ef9689a43d782fb2c9b2354a4ab9
marinebelin <marinebelin@free.fr>
2017-05-29 16:52:19 +0200

    fonctionnel !

--- js/contact_me.js
+++ js/contact_me.js
@@ -10,23 +10,19 @@ $(function() {
         submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
+            var titre = $("input#titre").val();
+            var auteur = $("input#auteur").val();
+            var soustitre = $("input#soustitre").val();
+            var contenu = $("textarea#contenu").val();
+           
             $.ajax({
                 url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
+                    titre: titre,
+                    auteur: auteur,
+                    soustitre: soustitre,
+                    contenu: contenu
                 },
                 cache: false,
                 success: function() {
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,35 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
+
+$servername = "localhost"; 
+$username = "bmarine";
+$password = "bmarine@2017";
+$dbname = "bmarine";
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // prepare sql and bind parameters
+            $stmt = $conn->prepare("INSERT INTO articles (titre, auteur, soustitre, contenu) 
+            VALUES (:titre, :auteur, :soustitre, :contenu)");  
+            $stmt->bindParam(':titre', $titre);
+            $stmt->bindParam(':auteur', $auteur);
+            $stmt->bindParam(':soustitre', $soustitre);
+            $stmt->bindParam(':contenu', $contenu);
+            // insert a row
+            $titre = $_POST["titre"];
+            $auteur = $_POST["auteur"];
+            $soustitre = $_POST["soustitre"];
+            $contenu = $_POST["contenu"];
+            $stmt->execute();
+            
+            }
+
+        catch(PDOException $e)
+            {
+             $error["bdd"] =  "Error: " . $e->getMessage();
+            }
+
+$conn = null;
    
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-   
-// Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
 ?>
